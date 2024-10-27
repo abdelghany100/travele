08f305c4471dd5f9678a48a51fe4ea9bebba1cfa
abdelghany100 <abdelghaniayman8@gmail.com>
2024-10-27 08:32:31 +0300

    indexLocation

--- models/Location.js
+++ models/Location.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const locationSchema = new mongoose.Schema([
+const locationSchema = new mongoose.Schema(
   {
-    country: { type: String, required: true, trim: true },
-    city: { type: String,  trim: true },
+    country: { type: String, required: true, trim: true, lowercase: true },
+    city: { type: String, trim: true, lowercase: true },
   },
-]);
+ 
+);
+locationSchema.index({ country: 1, city: 1 }, { unique: true })
 
 const Location = mongoose.model("Location", locationSchema);
 module.exports = {
